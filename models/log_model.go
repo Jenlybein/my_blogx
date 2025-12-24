b9ba23f4cc2afc13b8129a623a0cf90914de38c4
Jenlybein <865033582@qq.com>
2025-12-24 19:34:38 +0800

    feat:添加运行日志

--- models/log_model.go
+++ models/log_model.go
@@ -11,11 +11,12 @@ type LogModel struct {
 	Level       enum.LogLevelType `json:"level"`                        // 日志级别
 	UserID      uint              `json:"user_id"`                      // 用户ID
 	UserModel   UserModel         `json:"user_model" gorm:"foreignKey:UserID;references:ID"`
-	IP          string            `gorm:"size:32" json:"ip"`       // 操作IP
-	Addr        string            `gorm:"size:64" json:"addr"`     // 操作地址
-	IsRead      bool              `json:"is_read"`                 // 是否已读
-	LoginStatus bool              `json:"login_status"`            // 登录状态
-	Username    string            `gorm:"size:32" json:"username"` // 登录日志的用户名
-	Password    string            `gorm:"size:32" json:"password"` // 登录日志的密码
-	LoginType   enum.LoginType    `json:"login_type"`              // 登录类型
+	IP          string            `gorm:"size:32" json:"ip"`           // 操作IP
+	Addr        string            `gorm:"size:64" json:"addr"`         // 操作地址
+	IsRead      bool              `json:"is_read"`                     // 是否已读
+	LoginStatus bool              `json:"login_status"`                // 登录状态
+	Username    string            `gorm:"size:32" json:"username"`     // 登录日志的用户名
+	Password    string            `gorm:"size:32" json:"password"`     // 登录日志的密码
+	LoginType   enum.LoginType    `json:"login_type"`                  // 登录类型
+	ServiceName string            `gorm:"size:32" json:"service_name"` // 服务名称
 }
