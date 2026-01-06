ef4b7569f174c862f70df2a0882fc8a6f82be054
Jenlybein <865033582@qq.com>
2026-01-06 19:55:07 +0800

    feat:通过控制台注册用户

---
+++ flags/flag_user/enter.go
@@ -0,0 +1,79 @@
+package flag_user
+
+import (
+	"fmt"
+	"myblogx/global"
+	"myblogx/models"
+	"myblogx/models/enum"
+	"myblogx/utils/pwd"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+type FlagUser struct {
+}
+
+func (FlagUser) Create() {
+	var role enum.RoleType
+	fmt.Println("请输入数字选择用户角色: ")
+	for r := 1; r <= enum.RoleTypeCount; r++ {
+		fmt.Printf("%d. %s\n", r, enum.RoleType(r).String())
+	}
+	if _, err := fmt.Scanln(&role); err != nil {
+		fmt.Printf("输入类型错误 %s\n", err.Error())
+		return
+	}
+	if int(role) < int(enum.RoleAdmin) || int(role) > enum.RoleTypeCount {
+		fmt.Printf("输入角色类型错误 %d\n", role)
+		return
+	}
+
+	var username string
+	fmt.Println("请输入用户名: ")
+	fmt.Scanln(&username)
+
+	var model models.UserModel
+	if global.DB.Take(&model, "username = ?", username).Error == nil {
+		fmt.Printf("用户名 %s 已存在\n", username)
+		return
+	}
+
+	fmt.Println("请输入密码: ")
+	password, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Printf("读取密码错误 %s\n", err.Error())
+		return
+	}
+	fmt.Println("请确认密码: ")
+	rePassword, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		fmt.Printf("读取密码错误 %s\n", err.Error())
+		return
+	}
+	if string(password) != string(rePassword) {
+		fmt.Println("两次密码输入不一致")
+		return
+	}
+
+	// 密码加密
+	hashedPassword, err := pwd.GenerateFromPassword(string(password))
+	if err != nil {
+		fmt.Printf("密码加密错误 %s\n", err.Error())
+		return
+	}
+
+	// 创建用户
+	if err := global.DB.Create(&models.UserModel{
+		Username: username,
+		Nickname: "命令用户",
+		Password: hashedPassword,
+		Role:     role,
+	}).Error; err != nil {
+		fmt.Printf("创建用户错误 %s\n", err.Error())
+		return
+	}
+	msg := fmt.Sprintf("用户 %s 创建成功\n", username)
+	logrus.Info(msg)
+}
--- models/enum/role_type.go
+++ models/enum/role_type.go
@@ -3,7 +3,21 @@ package enum
 type RoleType int8
 
 const (
-	RoleAdmin RoleType = 1 // 管理员
-	RoleUser  RoleType = 2 // 普通用户
-	RoleGuest RoleType = 3 // 访客
+	RoleAdmin     RoleType    = iota + 1 // 管理员
+	RoleUser                             // 普通用户
+	RoleGuest                            // 访客
+	RoleTypeCount = int(iota)            // 角色类型总数
 )
+
+func (r RoleType) String() string {
+	switch r {
+	case RoleAdmin:
+		return "管理员"
+	case RoleUser:
+		return "普通用户"
+	case RoleGuest:
+		return "访客"
+	default:
+		return "未知角色"
+	}
+}
