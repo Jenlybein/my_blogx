0f174d64d88acc857e16a6ba5bb66bbccfb839d2
Jenlybein <865033582@qq.com>
2026-03-21 20:08:43 +0800

    feat:用跳过tls验证的方法修复mysqldump依赖

--- init/deploy/blog_server/mariadb-dump-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-if command -v mariadb-dump >/dev/null 2>&1; then
-	exec "$(command -v mariadb-dump)" --skip-ssl-verify-server-cert "$@"
-fi
-
-exec "$(command -v mysqldump)" --skip-ssl-verify-server-cert "$@"
